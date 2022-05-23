ea7015dfb2f8a33eb8cd2710d39f8cdb767e359f
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-05-23 12:13:53 -0300

    Code adjustment
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- HashTable.cpp
+++ HashTable.cpp
@@ -23,55 +23,57 @@
 using namespace std;
 
 class HashTable {
+
 private:
+
     list<int> *table;
     int total_elements;
 
-    // Hash function to calculate hash for a value:
-    int getHash(int key) {
+    /** Hash function to calculate hash for a value: */
+    int getHash(int key) const {
         return key % total_elements;
     }
 
 public:
- 
-   int colisoes;  //contador de colisões
 
-  // Constructor to create a hash table with 'n' indices:
-  HashTable(int n){
-    total_elements = n;
-    table = new list<int>[total_elements];
-  }
+    int collisionCount = 0;
 
-  // Insert data in the hash table:
-  void insertElement(int key){
-    if(table[getHash(key)].size()>=1){
-      colisoes+=1;
-    }  //verifica se a posição já está ocupada. Se estiver, ocorreu uma colisão e somamos 1 às colisões
-    
-    table[getHash(key)].push_back(key); //Add element at the end
-  }
+    /** Constructor to create a hash table with 'n' indices: */
+    explicit HashTable(int n) {
+        total_elements = n;
+        table = new list<int>[total_elements];
+    }
 
-    // Remove data from the hash table:
+    /** Insert data in the hash table: */
+    void insertElement(int key) {
+        if (!table[getHash(key)].empty()) {
+            collisionCount += 1;
+        } /** Checks if the position is already occupied. If a collision has occurred and we add 1 to the collisionCount */
+
+        table[getHash(key)].push_back(key); /** Add element at the end */
+    }
+
+    /** Remove data from the hash table: */
     void removeElement(int key) {
         int x = getHash(key);
 
         list<int>::iterator i;
         for (i = table[x].begin(); i != table[x].end(); i++) {
-            // Check if the iterator points to the required item:
+            /** Check if the iterator points to the required item: */
             if (*i == key)
                 break;
         }
 
-        // If the item was found in the list, then delete it:
+        /** If the item was found in the list, then delete it: */
         if (i != table[x].end())
-            table[x].erase(i); //Erase elements
+            table[x].erase(i);
     }
 
     void printAll() {
-        // Traverse each index:
+        /** Traverse each index: */
         for (int i = 0; i < total_elements; i++) {
             cout << "Index " << i << ": ";
-            // Traverse the list at current index:
+            /** Traverse the list at current index: */
             for (int j: table[i])
                 cout << j << " => ";
 
@@ -81,24 +83,25 @@ public:
 };
 
 int main() {
-    // Create a hash table with x indices:
-    HashTable ht(9);
+    /** Create a hash table with x indices: */
+    HashTable hashTable(9);
 
-    // Declare the data to be stored in the hash table:
+    /** Declare the data to be stored in the hash table: */
     int array[] = {2, 4, 6, 8, 10, 1, 21, 13, 15, 9, 12, 30, 7, 17, 24, 45, 41, 32, 38, 39, 35, 26, 27, 50, 55, 52};
 
-    // Insert the whole data into the hash table:
-    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
-        ht.insertElement(array[i]);
+    /** Insert the whole data into the hash table: */
+    for (int i: array)
+        hashTable.insertElement(i);
 
-    cout << "\n..:: Hash Table ::.." << endl;
-    ht.printAll();
+    cout << "\n..........:: Hash Table ::............\n" << endl;
+    hashTable.printAll();
 
-    ht.removeElement(1);
-    cout << endl << "..:: After deleting element 1 ::.." << endl;
-    ht.printAll();
+    hashTable.removeElement(1);
+    cout << endl << "\n....:: After deleting element 1 ::....\n" << endl;
+    hashTable.printAll();
 
-  cout << "\nTotal de colisões: " << ht.colisoes << endl;
+    cout << "\n.........:: Collisions ::............" << endl;
+    cout << "\nTotal collisions: " << hashTable.collisionCount << endl;
 
     return 0;
 }
